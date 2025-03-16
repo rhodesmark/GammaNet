949032d056374483941839b4847ee392d4abe3ab
Mark Rhodes <rhodesy82@hotmail.co.uk>
2025-03-16 21:53:44 +0000

    Add files via upload

---
+++ GammaNet-Masterv3.0.0/GN-Masterv3.0.0/GammaNet_CodeV3.0.1/settings.h
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+
+const String debugMode = "On";
+const String SystemName = "GammaNet";
+const String CodeVersion = "3.0.0";
+const String CodeDeveloper = "Mark Rhodes";
+//Node Information
+const String DeviceType = "ESP32-32";
+const String NodeType = "Central Hub"; //Central Hub, Node Centre, Sensor Node, Repeater Node, Scatter Node.
+const String NodeID = "CH001";
+const String NodeStyle = "External"; //Internal, External
+//Internet Connection Details
+const String WebURL = "https://dataIn.gammanet.co.uk";
+const String phpFile1 = "dataSubmit.php";
+const String fullURL1 = "https://dataIn.gammanet.co.uk/dataSubmit.php";
